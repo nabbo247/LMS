e14a6a8ed82809ff66b2a3a6fbd03a07966e2edf
Dinesh Gabhane <dinesh.gabhane@hotmail.com>
2019-01-29 17:16:20 +0530

    Added new Class

---
+++ LMS/LMSBL/ExecuteSPs.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using LMSBL.DBModels;
+
+namespace LMSBL
+{
+    public class ExecuteSPs
+    {
+        static string ConnectionString = "";
+        SqlConnection con = new SqlConnection(ConnectionString);
+        public ExecuteSPs()
+        {
+
+        }
+        public Response ExecuteGetSp(string SPName, List<Param> Params, string ModelType)
+        {
+            Response response = new Response();
+            try
+            {
+                SqlDataReader rdr = null;
+
+                SqlCommand cmd = new SqlCommand(SPName, con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (var param in Params)
+                {
+                    cmd.Parameters.Add(new SqlParameter(param.Name, param.Value));
+                }
+                con.Open();
+                rdr = cmd.ExecuteReader();
+                switch (ModelType)
+                {
+                    case "tblTenant":
+                        response.lstTenants = BindTenantData(rdr);
+                        break;                    
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+            return response;
+        }
+        public static List<tblTenant> BindTenantData(SqlDataReader dr)
+        {
+            List<tblTenant> lstTenants = new List<tblTenant>();
+            while (dr.Read())
+            {
+                tblTenant objTenant = new tblTenant();
+                objTenant.tenantId = Convert.ToInt32(dr["tenantId"]);
+                objTenant.tenantName = Convert.ToString(dr["tenantName"]);
+                objTenant.tenantDomain = Convert.ToString(dr["tenantDomain"]);
+                objTenant.activationFrom = Convert.ToDateTime(dr["activationFrom"]);
+                objTenant.activationTo = Convert.ToDateTime(dr["activationTo"]);
+                objTenant.isActive = Convert.ToBoolean(dr["isActive"]);
+                objTenant.createdBy = Convert.ToInt32(dr["createdBy"]);
+                objTenant.createdOn = Convert.ToDateTime(dr["createdOn"]);
+                objTenant.noOfUserAllowed = Convert.ToInt32(dr["noOfUserAllowed"]);
+                lstTenants.Add(objTenant);
+            }
+            return lstTenants;
+        }
+    }
+}
